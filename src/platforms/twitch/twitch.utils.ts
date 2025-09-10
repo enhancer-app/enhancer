8223758681630c654c265af9ded8d4aafac283bb
igorovh <xoxo@igor.ovh>
2025-09-11 00:00:40 +0200

    fix: moderator view sometimes has # in the end of the url

--- src/platforms/twitch/twitch.utils.ts
+++ src/platforms/twitch/twitch.utils.ts
@@ -29,7 +29,8 @@ export default class TwitchUtils {
 		let name = elements[1];
 		if (name === "popout" || elements[0].includes("dashboard")) name = elements[2];
 		if (name === "moderator" || elements[0].includes("dashboard")) name = elements[2];
-		if (name.includes("?")) name = name.substring(0, name.indexOf("?"));
+		if (name.endsWith("?")) name = name.substring(0, name.indexOf("?"));
+		if (name.endsWith("#")) name = name.substring(0, name.indexOf("#"));
 		return name.toLowerCase();
 	}
 
