c437b68a2fc6c1b946909acb0ed34ec8c41fb03d
Kaedriz <kaedriz@proton.me>
2026-01-07 20:25:49 +0100

    refactor: Separate Reducer logic to separate module

--- src/platforms/kick/modules/settings/settings.module.tsx
+++ src/platforms/kick/modules/settings/settings.module.tsx
@@ -51,6 +51,10 @@ export default class SettingsModule extends KickModule {
 				iconUrl: await this.commonUtils().getAssetFile(this.workerService(), "settings/channel.svg"),
 			},
 			{
+				title: "Latency",
+				iconUrl: await this.commonUtils().getAssetFile(this.workerService(), "settings/latency.svg"),
+			},
+			{
 				title: "Experimental",
 				iconUrl: await this.commonUtils().getAssetFile(this.workerService(), "settings/experimental.svg"),
 			},
@@ -67,46 +71,6 @@ export default class SettingsModule extends KickModule {
 			discord: await this.commonUtils().getAssetFile(this.workerService(), "brands/discord.svg"),
 		} as const;
 		this.SETTING_DEFINITIONS = [
-			{
-				id: "streamLatencyEnabled",
-				title: "Enable Stream Latency",
-				description: "Shows the current stream delay on top of the chat.",
-				type: "toggle",
-				tabIndex: tabIndexes.General,
-				requiresRefreshToDisable: true,
-			},
-			{
-				id: "streamLatencyReducerEnabled",
-				title: "Enable Stream Latency Reducer",
-				description: "Reduces stream latency by adjusting playback rate.",
-				type: "toggle",
-				tabIndex: tabIndexes.General,
-				requiresRefreshToDisable: true,
-			},
-			{
-				id: "streamLatencyReducerMinRate",
-				title: "Minimum Playback Rate",
-				description: "The minimum playback rate the stream will be speeded up to.",
-				type: "number",
-				tabIndex: tabIndexes.General,
-				requiresRefreshToDisable: true,
-			},
-			{
-				id: "streamLatencyReducerMaxRate",
-				title: "Maximum Playback Rate",
-				description: "The maximum playback rate the stream will be speeded up to.",
-				type: "number",
-				tabIndex: tabIndexes.General,
-				requiresRefreshToDisable: true,
-			},
-			{
-				id: "streamLatencyReducerThreshold",
-				title: "Latency Threshold",
-				description: "The latency threshold (in seconds) at which the playback rate will be speeded up.",
-				type: "number",
-				tabIndex: tabIndexes.General,
-				requiresRefreshToDisable: true,
-			},
 			{
 				id: "realVideoTimeEnabled",
 				title: "Enable Real Video Time",
@@ -198,6 +162,56 @@ export default class SettingsModule extends KickModule {
 					return <WatchtimeListComponent platform="kick" workerService={workerService} />;
 				},
 				hideInfo: true,
+			},
+			{
+				id: "streamLatencyEnabled",
+				title: "Enable Stream Latency",
+				description: "Shows the current stream delay on top of the chat.",
+				type: "toggle",
+				tabIndex: tabIndexes.Latency,
+				requiresRefreshToDisable: true,
+			},
+			{
+				id: "streamLatencyReducerEnabled",
+				title: "Enable Stream Latency Reducer",
+				description: "Reduces stream latency by adjusting playback rate. (Disabled without Low Latency Mode)",
+				type: "toggle",
+				tabIndex: tabIndexes.Latency,
+				requiresRefreshToDisable: true,
+			},
+			{
+				id: "streamLatencyReducerMinRate",
+				title: "Minimum Playback Rate",
+				description: "The minimum playback rate the stream will be speeded up to.",
+				type: "number",
+				tabIndex: tabIndexes.Latency,
+				requiresRefreshToDisable: true,
+			},
+			{
+				id: "streamLatencyReducerMaxRate",
+				title: "Maximum Playback Rate",
+				description: "The maximum playback rate the stream will be speeded up to.",
+				type: "number",
+				tabIndex: tabIndexes.Latency,
+				requiresRefreshToDisable: true,
+			},
+			{
+				id: "streamLatencyReducerMinThreshold",
+				title: "Minimum Latency Threshold",
+				description:
+					"The latency threshold (in seconds) at which the playback rate will be speeded up to the minimum rate.",
+				type: "number",
+				tabIndex: tabIndexes.Latency,
+				requiresRefreshToDisable: true,
+			},
+			{
+				id: "streamLatencyReducerMaxThreshold",
+				title: "Maximum Latency Threshold",
+				description:
+					"The latency threshold (in seconds) at which the playback rate will be speeded up to the maximum rate.",
+				type: "number",
+				tabIndex: tabIndexes.Latency,
+				requiresRefreshToDisable: true,
 			},
 			{
 				id: "about",
---
+++ src/platforms/kick/modules/stream-latency-reducer/stream-latency-reducer.module.tsx
@@ -0,0 +1,116 @@
+import KickModule from "$kick/kick.module.ts";
+import { LatencyComponent } from "$shared/components/latency/latency.component.tsx";
+import type { KickModuleConfig } from "$types/shared/module/module.types.ts";
+import { signal } from "@preact/signals";
+import { render } from "preact";
+
+export default class StreamLatencyModule extends KickModule {
+	private updateInterval: NodeJS.Timeout | undefined;
+	private latencyTimings = signal<number[]>([]);
+
+	readonly config: KickModuleConfig = {
+		name: "stream-latency-reducer",
+		appliers: [
+			{
+				type: "selector",
+				key: "stream-latency-reducer",
+				selectors: ["#channel-chatroom"],
+				callback: this.run.bind(this),
+				validateUrl: (url) => {
+					return !url.includes("/videos/") && !url.includes("/clips/");
+				},
+				once: true,
+			},
+		],
+		isModuleEnabledCallback: async () => await this.settingsService().getSettingsKey("streamLatencyReducerEnabled"),
+	};
+
+	private run(): void {
+		if (this.updateInterval) clearInterval(this.updateInterval);
+		this.updateInterval = setInterval(async () => {
+			const status = await this.getPlaybackRateStatus();
+
+			if (status === "catchingUpMax") {
+				this.setPlaybackRateMode("catchUpMax");
+			} else if (status === "catchingUpMin") {
+				this.setPlaybackRateMode("catchUpMin");
+			} else {
+				this.setPlaybackRateMode("reset");
+			}
+		}, 1000);
+	}
+
+	private changePlaybackSpeed(video: HTMLVideoElement, rate: number) {
+		video.playbackRate = rate;
+	}
+
+	private async setPlaybackRateMode(mode: "catchUpMin" | "catchUpMax" | "reset") {
+		const videoPlayer = this.getPlayer();
+		if (!videoPlayer) return;
+
+		let targetRate = 1;
+		const latency = this.getLatency(videoPlayer);
+		if (!latency) return;
+
+		const minRate = (await this.getSettings()).minRate;
+		const maxRate = (await this.getSettings()).maxRate;
+		const minSpeedThreshold = (await this.getSettings()).minThreshold;
+		const maxSpeedThreshold = (await this.getSettings()).maxThreshold;
+
+		if (mode === "catchUpMax") {
+			targetRate = maxRate;
+		} else if (mode === "catchUpMin") {
+			targetRate =
+				minRate + ((maxRate - minRate) * (latency - minSpeedThreshold)) / (maxSpeedThreshold - minSpeedThreshold);
+		}
+
+		this.changePlaybackSpeed(videoPlayer, targetRate);
+	}
+
+	private async getPlaybackRateStatus() {
+		const videoPlayer = this.getPlayer();
+		if (!videoPlayer) return;
+		const latency = this.getLatency(videoPlayer);
+		if (!latency) return "invalid";
+
+		const { maxThreshold, minThreshold } = await this.getSettings();
+		if (latency >= Math.abs(maxThreshold)) return "catchingUpMax";
+		if (latency > Math.abs(minThreshold)) return "catchingUpMin";
+		if (latency <= Math.abs(minThreshold)) return "caughtUp";
+		return "invalid";
+	}
+
+	private async getSettings() {
+		const minRate = await this.settingsService().getSettingsKey("streamLatencyReducerMinRate");
+		const maxRate = await this.settingsService().getSettingsKey("streamLatencyReducerMaxRate");
+		const minThreshold = await this.settingsService().getSettingsKey("streamLatencyReducerMinThreshold");
+		const maxThreshold = await this.settingsService().getSettingsKey("streamLatencyReducerMaxThreshold");
+		return { minRate, maxRate, minThreshold, maxThreshold };
+	}
+
+	// Calculates latency based on
+	private getLatency(video: HTMLVideoElement): number {
+		const { currentTime, buffered } = video;
+		if (buffered.length === 0) return -1;
+		const bufferEnd = buffered.end(buffered.length - 1);
+
+		this.latencyTimings.value.push(bufferEnd - currentTime);
+
+		if (!this.updateInterval) return 0;
+		const numberOfSamples = 10;
+		if (this.latencyTimings.value.length > numberOfSamples) this.latencyTimings.value.shift();
+
+		return (
+			this.latencyTimings.value.reduce((accumulator, currentValue) => accumulator + currentValue, 0) /
+			this.latencyTimings.value.length
+		);
+	}
+
+	private getPlayer() {
+		const video = document.querySelector("video");
+		if (!video || !this.kickUtils().isLiveVideo(video)) {
+			return null;
+		}
+		return video;
+	}
+}
