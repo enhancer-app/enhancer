0720d8ff9db3f0de03e34b798f38786b7d7eb32f
igor <37638480+igorovh@users.noreply.github.com>
2026-03-29 20:16:09 +0200

    feat: funny update (#158)
    * feat: funny update
    
    * more funny things
    
    * fixes
    
    * fixes
    
    * add funny things final
    
    * fixes
    
    * fixes
    
    * fixes

---
+++ src/platforms/twitch/modules/funny-module/funny.module.tsx
@@ -0,0 +1,85 @@
+import {
+	FUNNY_AVATARS,
+	FUNNY_CATEGORIES,
+	FUNNY_NAMES,
+	FUNNY_TITLES,
+	FUNNY_TOOLTIPS,
+} from "$shared/funny-thing/funny-things.ts";
+import TwitchModule from "$twitch/twitch.module.ts";
+import type { TwitchModuleConfig } from "$types/shared/module/module.types.ts";
+
+export default class FunnyModule extends TwitchModule {
+	readonly config: TwitchModuleConfig = {
+		name: "funny-module",
+		appliers: [
+			{
+				type: "selector",
+				selectors: ['#side-nav .side-nav-section .side-nav-card__link[data-test-selector="followed-channel"]'],
+				callback: (elements) => elements.forEach((element) => this.replaceFollowedChannel(element)),
+				key: "funny-module-replace-follows",
+			},
+			{
+				type: "selector",
+				selectors: ["section#live-channel-stream-information"],
+				callback: (elements) => elements.forEach((element) => this.replaceChannelInfo(element)),
+				key: "funny-module-replace-channel-info",
+			},
+		],
+		isModuleEnabledCallback: async () => {
+			return (await this.settingsService().getSettingsKey("_funnyThings")) && this.commonUtils().isFunnyDay();
+		},
+	};
+
+	private replaceFollowedChannel(element: Element) {
+		const url = (element as HTMLLinkElement).href;
+		const originalName = url.substring(url.lastIndexOf("/") + 1);
+		const funnyName = FUNNY_NAMES[originalName];
+		const funnyAvatar = FUNNY_AVATARS[originalName];
+		const funnyTooltip = FUNNY_TOOLTIPS[originalName] ?? "was definitely not changed by Enhancer";
+
+		const username = element.querySelector<HTMLElement>('p[data-a-target="side-nav-title"]');
+		if (username && funnyName) {
+			username.textContent = funnyName;
+			username.title = `Name ${funnyTooltip}. You definitely can't disable this in Enhancer settings.`;
+		}
+
+		const avatar = element.querySelector<HTMLImageElement>(".side-nav-card__avatar img");
+		if (avatar && funnyAvatar) {
+			avatar.src = funnyAvatar;
+			avatar.title = `Avatar ${funnyTooltip}. You definitely can't disable this in Enhancer settings.`;
+		}
+	}
+
+	private replaceChannelInfo(element: Element) {
+		const usernameElement = element.querySelector<HTMLElement>(".metadata-layout__support h1");
+		const username = usernameElement?.textContent?.toLowerCase() ?? "";
+		const funnyName = FUNNY_NAMES[username];
+		const funnyAvatar = FUNNY_AVATARS[username];
+		const funnyTitle = FUNNY_TITLES[username];
+		const funnyCategory = FUNNY_CATEGORIES[username];
+		const funnyTooltip = FUNNY_TOOLTIPS[username] ?? "was definitely not changed by Enhancer";
+
+		if (usernameElement && funnyName) {
+			usernameElement.textContent = funnyName;
+			usernameElement.title = `Name ${funnyTooltip}. You definitely can't disable this in Enhancer settings.`;
+		}
+
+		const avatar = element.querySelector<HTMLImageElement>("img.tw-image-avatar");
+		if (avatar && funnyAvatar) {
+			avatar.src = funnyAvatar;
+			avatar.title = `Avatar ${funnyTooltip}. You definitely can't disable this in Enhancer settings.`;
+		}
+
+		const title = element.querySelector<HTMLElement>('p[data-a-target="stream-title"]');
+		if (title && funnyTitle) {
+			title.textContent = funnyTitle;
+			title.title = `Title ${funnyTooltip}. You definitely can't disable this in Enhancer settings.`;
+		}
+
+		const category = element.querySelector<HTMLElement>('a[data-a-target="stream-game-link"]');
+		if (category && funnyCategory) {
+			category.textContent = funnyCategory;
+			category.title = `Category ${funnyTooltip}. You definitely can't disable this in Enhancer settings.`;
+		}
+	}
+}
